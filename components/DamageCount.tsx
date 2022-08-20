db338479f0a9f2a6f3b16cf436e6afe844acb604
Chris Risner <crisner.prsvr@gmail.com>
2022-08-20 15:52:07 -0500

    resized DamageCount circle to 7

--- components/DamageCount.tsx
+++ components/DamageCount.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const DamageCount = ({ total }: Props) => {
   return (
-    <div className="ml-1 h-8 w-8 bg-red-600 flex items-center justify-center rounded-full text-gray-1 font-semibold text-xs animate-pulse transtion-all duration-150 ease-in">
+    <div className="ml-1 h-7 w-7 bg-red-600 flex items-center justify-center rounded-full text-gray-1 font-semibold text-xs animate-pulse transtion-all duration-150 ease-in">
       {total}
     </div>
   );
