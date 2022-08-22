6f77b2a5a87a1c432f3de27b9b02668395c55404
Chris Risner <crisner.prsvr@gmail.com>
2022-08-22 07:42:33 -0500

    modified formatVehicleDetails and modified AutoGrade with functions for text and bg colors

--- components/AutoGrade.tsx
+++ components/AutoGrade.tsx
@@ -1,5 +1,6 @@
 import { BadgeCheckIcon } from "@heroicons/react/outline";
 import { Vehicle } from "typings";
+import { newBgColor, newTextColor } from "utils/formatGrade";
 
 interface Props {
   vehicle: Vehicle;
@@ -10,25 +11,13 @@ const AutoGrade = ({ vehicle: { grade } }: Props) => {
     <div className="flex flex-col items-center space-y-1 pt-4">
       <div className="relative flex flex-col items-center">
         <BadgeCheckIcon
-          className={`h-16 w-16 ${
-            grade?.value > 0 && grade?.value < 3
-              ? "text-dirty-yellow"
-              : grade?.value > 3
-              ? "text-green-accent"
-              : "text-gray-6"
-          }`}
+          className={`h-16 w-16 ${newTextColor(grade?.value)}`}
         />
         <p className="text-xs font-monty text-gray-6 leading-none -mt-0.5">
           {grade?.description}
         </p>
         <div
-          className={`${
-            grade?.value > 0 && grade?.value < 3
-              ? "bg-dirty-yellow"
-              : grade?.value > 3
-              ? "bg-green-accent"
-              : "bg-dark-1 dark:bg-gray-1"
-          } absolute top-4 right-[16px] h-8 w-8 rounded-full flex items-center justify-center`}>
+          className={`${newBgColor(grade?.value)} absolute top-4 right-[16px] h-8 w-8 rounded-full flex items-center justify-center`}>
           <span className="text-white text-xs font-semibold">
             {grade?.value}
           </span>
