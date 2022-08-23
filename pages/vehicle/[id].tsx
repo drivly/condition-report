e0de8a6ebd1171920ba58e908e074aaee026e712
Chris Risner <crisner.prsvr@gmail.com>
2022-08-23 17:35:36 -0500

    had to remove a div

--- pages/vehicle/[id].tsx
+++ pages/vehicle/[id].tsx
@@ -27,32 +27,29 @@ const VehiclePage = ({ vehicle }: Props) => {
 
       <main className="mx-auto max-w-5xl grid grid-cols-1 md:gap-8 md:grid-cols-12 py-12 mb-8">
         <section className="col-span-1 md:col-span-7">
-        <div className="md:sticky md:top-[121px] space-y-16">
-          {/* Left Side*/}
-          <ImageGallery vehicle={vehicle} sectionStyle="mb-16" />
-          {/* Show on Mobile up to md */}
-          <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 md:hidden" />
-          <VehicleDetails
-            vehicle={vehicle}
-            sectionStyle="md:hidden mb-16 bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md"
-          />
-          <div className="bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md">
-            <SectionHeader vehicle={vehicle} sectionStyle="mb-8" />
-            <InspectionCard vehicle={vehicle} />
+          <div className="md:sticky md:top-[121px] space-y-16">
+            {/* Left Side*/}
+            <ImageGallery vehicle={vehicle} sectionStyle="mb-16" />
+            {/* Show on Mobile up to md */}
+            <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 md:hidden" />
+            <VehicleDetails
+              vehicle={vehicle}
+              sectionStyle="md:hidden mb-16 bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md"
+            />
+            <div className="bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md">
+              <SectionHeader vehicle={vehicle} sectionStyle="mb-8" />
+              <InspectionCard vehicle={vehicle} />
+            </div>
           </div>
-        </div>
-          
         </section>
         {/* Vehicle Title, Details & Annnouncements */}
         <section className="col-span-1 md:col-span-5">
-          <div className="md:sticky md:top-[137px] space-y-16">
-            <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 hidden md:flex" />
-            <VehicleDetails
-              vehicle={vehicle}
-              sectionStyle="hidden md:flex bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 md:rounded-md"
-            />
-            <Announcements vehicle={vehicle} />
-          </div>
+          <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 hidden md:flex" />
+          <VehicleDetails
+            vehicle={vehicle}
+            sectionStyle="hidden md:flex bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 md:rounded-md"
+          />
+          <Announcements vehicle={vehicle} />
         </section>
       </main>
     </div>
