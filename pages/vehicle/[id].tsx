f30d8eb9840d8ba2847da8ed7e37adec4262ae5f
Chris Risner <crisner.prsvr@gmail.com>
2022-08-23 17:40:04 -0500

    added sticky divs to both right and left side sections

--- pages/vehicle/[id].tsx
+++ pages/vehicle/[id].tsx
@@ -27,29 +27,32 @@ const VehiclePage = ({ vehicle }: Props) => {
 
       <main className="mx-auto max-w-5xl grid grid-cols-1 md:gap-8 md:grid-cols-12 py-12 mb-8">
         <section className="col-span-1 md:col-span-7">
-          <div className="md:sticky md:top-[121px] space-y-16">
-            {/* Left Side*/}
-            <ImageGallery vehicle={vehicle} sectionStyle="mb-16" />
-            {/* Show on Mobile up to md */}
-            <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 md:hidden" />
-            <VehicleDetails
-              vehicle={vehicle}
-              sectionStyle="md:hidden mb-16 bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md"
-            />
-            <div className="bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md">
-              <SectionHeader vehicle={vehicle} sectionStyle="mb-8" />
-              <InspectionCard vehicle={vehicle} />
-            </div>
+        <div className="md:sticky md:top-[121px] space-y-16">
+          {/* Left Side*/}
+          <ImageGallery vehicle={vehicle} sectionStyle="mb-16" />
+          {/* Show on Mobile up to md */}
+          <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 md:hidden" />
+          <VehicleDetails
+            vehicle={vehicle}
+            sectionStyle="md:hidden mb-16 bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md"
+          />
+          <div className="bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 rounded-md">
+            <SectionHeader vehicle={vehicle} sectionStyle="mb-8" />
+            <InspectionCard vehicle={vehicle} />
           </div>
+        </div>
+          
         </section>
         {/* Vehicle Title, Details & Annnouncements */}
         <section className="col-span-1 md:col-span-5">
-          <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 hidden md:flex" />
-          <VehicleDetails
-            vehicle={vehicle}
-            sectionStyle="hidden md:flex bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 md:rounded-md"
-          />
-          <Announcements vehicle={vehicle} />
+          <div className="md:sticky md:top-[121px] space-y-16">
+            <VehicleTitle vehicle={vehicle} sectionStyle="mb-8 hidden md:flex" />
+            <VehicleDetails
+              vehicle={vehicle}
+              sectionStyle="hidden md:flex bg-white dark:bg-black-2 shadow-md py-8 px-4 lg:px-6 md:rounded-md"
+            />
+            <Announcements vehicle={vehicle} />
+          </div>
         </section>
       </main>
     </div>
